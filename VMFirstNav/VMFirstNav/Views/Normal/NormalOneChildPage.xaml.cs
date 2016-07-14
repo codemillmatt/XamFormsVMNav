342d5b07d7ec4b719a110c370177f13fe20f7d8c
Matt Soucoup <msoucoup@codemilltech.com>
2016-07-14 04:32:32 -0500

    Adding the "normal" pages

---
+++ VMFirstNav/VMFirstNav/Views/Normal/NormalOneChildPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace VMFirstNav
+{
+	public partial class NormalOneChildPage : ContentPage
+	{
+		public NormalOneChildPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
