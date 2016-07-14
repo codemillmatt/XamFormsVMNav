d5c739b588295977552a53ea1f558a60305d9b47
Matt Soucoup <msoucoup@codemilltech.com>
2016-07-13 20:15:10 -0500

    Getting the nav service to work

---
+++ VMFirstNav/VMFirstNav/ViewModels/MasterDetail/MasterListNavViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using MvvmHelpers;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Splat;
+namespace VMFirstNav
+{
+	public class MasterListNavViewModel : BaseViewModel
+	{
+		INavigationService _navService;
+
+		// The overall list that will keep track of which view models can be navigated
+		// to and displayed in the "master" portion of master/detail
+		public List<IMasterListItem<BaseViewModel>> AvailablePages { get; set; }
+
+		public MasterListNavViewModel()
+		{
+			_navService = Locator.CurrentMutable.GetService<INavigationService>();
+
+			AvailablePages = new List<IMasterListItem<BaseViewModel>>();
+			AvailablePages.Add(new MasterListItem<TabOneViewModel>("Tab One"));
+			AvailablePages.Add(new MasterListItem<TabTwoViewModel>("Tab Two"));
+
+			Title = "Nav";
+		}
+
+		ICommand _navCommand;
+		public ICommand NavigateCommand
+		{
+			get
+			{
+				if (_navCommand == null)
+				{
+					_navCommand = new Command((selectedItem) =>
+					{
+						// Get the selected item from the command
+						var itemToNavigate = selectedItem as IMasterListItem<BaseViewModel>;
+
+						if (itemToNavigate != null)
+						{
+							// Get the view model type
+							var viewModelType = itemToNavigate.GetType().GenericTypeArguments[0];
+
+							// Get a view model instance
+							var viewModel = Activator.CreateInstance(viewModelType) as BaseViewModel;
+
+							// Perform the switch
+							_navService.SwitchDetailPage(viewModel);
+						}
+					});
+				}
+				return _navCommand;
+			}
+		}
+	}
+}
+
--- VMFirstNav/VMFirstNav/Views/RootTabPage.xaml.cs
+++ VMFirstNav/VMFirstNav/Views/RootTabPage.xaml.cs
@@ -2,18 +2,55 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using System.Diagnostics.Contracts;
 
 namespace VMFirstNav
 {
-	public partial class RootTabPage : TabbedPage
+	public partial class RootTabPage : TabbedPage, IViewFor<RootTabViewModel>
 	{
-		public RootTabPage ()
-		{
-			InitializeComponent ();
 
-			tabOne.ViewModel = new TabOneViewModel ();
-			tabTwo.ViewModel = new TabTwoViewModel ();
+		#region IViewFor
+
+		RootTabViewModel _vm;
+
+		public RootTabViewModel ViewModel
+		{
+			get
+			{
+				return _vm;
+			}
+
+			set
+			{
+				_vm = value;
+				BindingContext = _vm;
+			}
+		}
+
+		object IViewFor.ViewModel
+		{
+			get
+			{
+				return _vm;
+			}
+
+			set
+			{
+				ViewModel = (RootTabViewModel)value;
+			}
+		}
+		#endregion
+
+		public RootTabPage()
+		{
+			InitializeComponent();
+
+			tabOne.ViewModel = new TabOneViewModel();
+			tabTwo.ViewModel = new TabTwoViewModel();
 		}
 	}
+
+
+
 }
 
